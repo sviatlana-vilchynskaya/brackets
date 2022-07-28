d69003d016dac72ac9dbd88929cecd7da3a50495
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2022-07-28 13:51:45 +0300

    feat: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  while (true) {
+    let oldLength = str.length;
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      str = str.replace(bracketsConfig[i].join(""), "");
+    }
+
+    if (str.length === 0 || str.length === oldLength) {
+      break;
+    }
+  }
+
+  return str.length === 0;
+};
